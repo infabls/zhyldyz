b0d2b799aaa081c42348f5d4857b4e03ee3db90d
infabls <infabls.com@gmail.com>
2021-01-26 16:32:20 +0600

    add lottery archive migration and model

---
+++ app/Models/LotteriesArchive.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LotteriesArchive extends Model
+{
+	protected $fillable = ['id'];
+    use HasFactory;
+}
---
+++ database/migrations/2020_11_28_071825_create_lottery_archive_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLotteryArchiveTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lotteries_archive', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('urlKey');
+            $table->timestamp('starts_at')->nullable();
+            $table->timestamp('ends_at')->nullable();
+            $table->string('price');
+            $table->string('description');
+            $table->string('winner_user_id');
+            $table->string('winner_ticket_id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lottery');
+    }
+}
