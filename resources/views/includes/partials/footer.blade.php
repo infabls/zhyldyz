6b1de0a64cb286c1b8eb8cfc4a14c12e96bc6a02
infabls <infabls.com@gmail.com>
2021-01-26 18:01:00 +0600

    change footer text fields

--- resources/views/includes/partials/footer.blade.php
+++ resources/views/includes/partials/footer.blade.php
@@ -8,14 +8,14 @@
             <div class="pos-info">
               <ul class="row">
                 <li class="col-sm-4"> <i class="flaticon-route"></i>
-                  <h6>Postal Address</h6>
+                  <h6>Юридический адрес</h6>
                   <p>121 King Street Melbourne, Victoria 3000 Australia </p>
               </li>
               <li class="col-sm-4"> <i class="flaticon-mail"></i>
-                  <h6>General Enquiries</h6>
+                  <h6>Для запросов</h6>
                   <a href="mailto:demos@domainname.com">demos@domainname.com</a> <a href="mailto:demos@domainname.com">demos@domainname.com</a> </li>
                   <li class="col-sm-4"> <i class="flaticon-smartphone"></i>
-                      <h6>Business Phone</h6>
+                      <h6>Телефоны</h6>
                       <a href="tel:555-555-5555">001 123 456 7890</a> <a href="tel:555-555-5555">001 123 456 7890</a> </li>
                   </ul>
               </div>
@@ -27,7 +27,7 @@
   <div class="container">
     <div class="row">
       <div class="col-sm-6 text-left">
-        <p>2018 © M_Adnan.  All rights reserved.</p>
+        <p>2021 © dfw.kz  All rights reserved.</p>
     </div>
     <div class="col-sm-6"> <a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-behance"></i></a> </div>
 </div>
