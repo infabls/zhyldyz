8caf18845b2fd2c7d697906e8a8c8ee35c042dbc
infabls <infabls.com@gmail.com>
2021-01-26 22:44:03 +0600

    make some changes in header menu

--- resources/views/includes/partials/header.blade.php
+++ resources/views/includes/partials/header.blade.php
@@ -3,9 +3,9 @@
       <div class="container">
         <div class="logo"> 
           <!-- Main Nav Logo --> 
-          <a class="main-logo" href="index.html"><img src="/images/logo.png" alt=""></a> 
+          <a class="main-logo" href="/"><img src="/images/logo.png" alt=""></a> 
           <!-- Logo sticky --> 
-          <a class="logo-sticky" href="index.html"><img src="/images/logo.png" alt=""></a></div>
+          <a class="logo-sticky" href="/"><img src="/images/logo.png" alt=""></a></div>
           <!-- Nav -->
           <nav class="navbar ownmenu">
               <div class="navbar-header">
@@ -15,121 +15,6 @@
             <!-- NAV -->
             <div class="collapse navbar-collapse" id="nav-open-btn">
                 <ul class="nav">
-                  <li class="dropdown active"> <a href="/" class="dropdown-toggle" data-toggle="dropdown">Главная </a>
-{{--                     <ul class="dropdown-menu">
-                      <li><a href="index.html">Index Defult</a></li>
-                      <li><a href="index_freelancer.html">Index Freelancer</a></li>
-                      <li><a href="index_left_fix_menu.html">Index Left Fix Menu</a></li>
-                      <li><a href="index_portfolio.html">Index Portfolio</a></li>
-                      <li><a href="index_shop.html">Index Shop</a></li>
-                      <li><a href="index_full_nav.html">Index Full Nav 01</a></li>
-                      <li><a href="index_full_nav_1.html">Index Full Nav 02</a></li>
-                      <li><a href="index_menu_offcanvas.html">Index Right Menu </a></li>
-                      <li><a href="index_menu_offcanvas_left.html">Index Left Menu </a></li>
-                  </ul> --}}
-              </li>
-              <li class="dropdown"> <a href="index.html" class="dropdown-toggle" data-toggle="dropdown">Pages </a>
-                <ul class="dropdown-menu">
-                  <li class="dropdown-submenu"> <a href="index.html" class="dropdown-toggle" data-toggle="dropdown">Home</a>
-                    <ul class="dropdown-menu">
-                      <li><a href="index.html">Index Defult</a></li>
-                      <li><a href="index_freelancer.html">Index Freelancer</a></li>
-                      <li><a href="index_left_fix_menu.html">Index Left Fix Menu</a></li>
-                      <li><a href="index_portfolio.html">Index Portfolio</a></li>
-                      <li><a href="index_shop.html">Index Shop</a></li>
-                      <li><a href="index_full_nav.html">Index Full Nav 01</a></li>
-                      <li><a href="index_full_nav_1.html">Index Full Nav 02</a></li>
-                      <li><a href="index_menu_offcanvas.html">Index Right Menu </a></li>
-                      <li><a href="index_menu_offcanvas_left.html">Index Left Menu </a></li>
-                  </ul>
-              </li>
-              <li class="dropdown-submenu"> <a href="about_us.html" class="dropdown-toggle" data-toggle="dropdown">About</a>
-                <ul class="dropdown-menu">
-                  <li><a href="about_us.html">About Us</a></li>
-                  <li><a href="about_me.html">About Me</a></li>
-              </ul>
-          </li>
-          <li class="dropdown-submenu"> <a href="our_services.html" class="dropdown-toggle" data-toggle="dropdown">Services</a>
-            <ul class="dropdown-menu">
-              <li><a href="our_services.html">Our Services 01</a></li>
-              <li><a href="our_services_1.html">Our Services 02</a></li>
-          </ul>
-      </li>
-      <li class="dropdown-submenu"> <a href="#" class="dropdown-toggle" data-toggle="dropdown">Shop</a>
-        <ul class="dropdown-menu">
-          <li><a href="shop.html">Shop</a></li>
-          <li><a href="index_shop.html">Shop index</a></li>
-          <li><a href="shop-left-sidebar.html">Shop Left Sidebar</a></li>
-          <li><a href="shop-sidebar.html">Shop Right Sidebar</a></li>
-          <li><a href="shop-2-col-left-sidebar.html">Shop 2 Col Sidebar</a></li>
-          <li><a href="shop-sidebar.html">Shop 2 Col Right Sidebar</a></li>
-          <li><a href="shop-detail.html">Shop Detail</a></li>
-          <li><a href="shop-cart.html">Shop Cart</a></li>
-          <li><a href="shop-checkout.html">Shop Checkout</a></li>
-      </ul>
-  </li>
-  <li class="dropdown-submenu"> <a href="portfolio_1-4-col.html" class="dropdown-toggle" data-toggle="dropdown">Portfolio Style 1</a>
-    <ul class="dropdown-menu">
-      <li><a href="portfolio_1-2-col.html">Portfolio 2 Col</a></li>
-      <li><a href="portfolio_1-3-col.html">Portfolio 3 Col</a></li>
-      <li><a href="portfolio_1-4-col.html">Portfolio 4 Col</a></li>
-  </ul>
-</li>
-<li class="dropdown-submenu"> <a href="portfolio_2-4-col.html" class="dropdown-toggle" data-toggle="dropdown">Portfolio Style 2</a>
-    <ul class="dropdown-menu">
-      <li><a href="portfolio_2-2-col.html">Portfolio 2 Col</a></li>
-      <li><a href="portfolio_2-3-col.html">Portfolio 3 Col</a></li>
-      <li><a href="portfolio_2-4-col.html">Portfolio 4 Col</a></li>
-  </ul>
-</li>
-<li class="dropdown-submenu"> <a href="portfolio_3-4-col.html" class="dropdown-toggle" data-toggle="dropdown">Portfolio Style 3</a>
-    <ul class="dropdown-menu">
-      <li><a href="portfolio_3-2-col.html">Portfolio 2 Col</a></li>
-      <li><a href="portfolio_3-3-col.html">Portfolio 3 Col</a></li>
-      <li><a href="portfolio_3-4-col.html">Portfolio 4 Col</a></li>
-  </ul>
-</li>
-<li class="dropdown-submenu"> <a href="portfolio_4-4-col.html" class="dropdown-toggle" data-toggle="dropdown">Portfolio Style 4</a>
-    <ul class="dropdown-menu">
-      <li><a href="portfolio_4-2-col.html">Portfolio 2 Col</a></li>
-      <li><a href="portfolio_4-3-col.html">Portfolio 3 Col</a></li>
-      <li><a href="portfolio_4-4-col.html">Portfolio 4 Col</a></li>
-  </ul>
-</li>
-<li class="dropdown-submenu"> <a href="portfolio_5-4-col.html" class="dropdown-toggle" data-toggle="dropdown">Portfolio Style 5</a>
-    <ul class="dropdown-menu">
-      <li><a href="portfolio_5-2-col.html">Portfolio 2 Col</a></li>
-      <li><a href="portfolio_5-3-col.html">Portfolio 3 Col</a></li>
-      <li><a href="portfolio_5-4-col.html">Portfolio 4 Col</a></li>
-  </ul>
-</li>
-<li class="dropdown-submenu"> <a href="single_project_1.html" class="dropdown-toggle" data-toggle="dropdown">Portfolio Single</a>
-    <ul class="dropdown-menu">
-      <li><a href="single_project_1.html">Single Project 1</a></li>
-      <li><a href="single_project_2.html">Single Project 2</a></li>
-      <li><a href="single_project_3.html">Single Project 3</a></li>
-      <li><a href="single_project_4.html">Single Project 4</a></li>
-  </ul>
-</li>
-<li class="dropdown-submenu"> <a href="blog_classic.html" class="dropdown-toggle" data-toggle="dropdown">Blog</a>
-    <ul class="dropdown-menu">
-      <li><a href="blog.html">Blog Simple</a></li>
-      <li><a href="blog_masonry.html">Blog Masonry</a></li>
-      <li><a href="blog_sidebar.html">Blog Right Sidebar</a></li>
-      <li><a href="blog_left-sidebar.html">Blog Left Sidebar</a></li>
-      <li><a href="blog_single_post.html">Blog Single Post</a></li>
-  </ul>
-</li>
-<li class="dropdown-submenu"> <a href="contact_us_1.html" class="dropdown-toggle" data-toggle="dropdown">Contact</a>
-    <ul class="dropdown-menu">
-      <li><a href="contact_us_1.html">Contact Us 1</a></li>
-      <li><a href="contact_us_2.html">Contact Us 2</a></li>
-      <li><a href="contact_us_3.html">Contact Us 3</a></li>
-  </ul>
-</li>
-<li><a href="page_404.html">Page 404</a></li>
-</ul>
-</li>
 <li class="dropdown"> <a href="portfolio_1-2-col.html" class="dropdown-toggle" data-toggle="dropdown">Portfolio </a>
     <ul class="dropdown-menu">
       <li class="dropdown-submenu"> <a href="portfolio_1-4-col.html" class="dropdown-toggle" data-toggle="dropdown">Portfolio Style 1</a>
@@ -179,7 +64,9 @@
 </li>
 <!-- Mega Menu Nav -->
 <li class="dropdown megamenu"> <a href="/lotteries" >Все лотереи </a></li>
-<li class="dropdown"> <a href="/lottery/1">Лотерея 1 </a></li>
+<li class="dropdown"> <a href="/lottery/1">Лотерея 100 </a></li>
+<li class="dropdown"> <a href="/lottery/6">Лотерея 500 </a></li>
+<li class="dropdown"> <a href="/lottery/3">Лотерея 1000 </a></li>
 <li class="dropdown"> <a href="blog.html" class="dropdown-toggle" data-toggle="dropdown">Blog </a>
     <ul class="dropdown-menu">
       <li><a href="blog.html">Blog Simple</a></li>
@@ -204,25 +91,10 @@
     <ul>
       <li>
         <div class="fly-nac-icon for-all">
-          <div class="toggle-button" id="toggle"> <span class="bar top"></span> <span class="bar middle"></span> <span class="bar bottom"></span> </div>
+          <a id="cd-menu-trigger" href="#0"><span class="cd-menu-icon"></span></a>
       </div>
   </li>
-  <li> <a href="javascript:void(0);" class="search-open"><i class="fa fa-search"></i></a>
-    <div class="search-inside animated-4s fadeIn"> <i class="lnr lnr-cross search-close"></i>
-      <div class="search-overlay"></div>
-      <div class="position-center-center">
-        <div class="container">
-          <div class="search">
-            <form>
-              <input type="search" placeholder="Type Your Search...">
-              <button type="submit"><i class="lnr lnr-magnifier"></i></button>
-          </form>
-      </div>
-  </div>
-</div>
-</div>
-</li>
-</ul>
+  </ul>
 </div>
 </nav>
 </div>
