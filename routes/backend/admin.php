0243113c8a18ab42dd1ad8b454e3964de2279056
infabls <infabls.com@gmail.com>
2021-01-26 16:54:22 +0600

    add current logic to draw funct

--- routes/backend/admin.php
+++ routes/backend/admin.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Backend\DashboardController;
 use Tabuna\Breadcrumbs\Trail;
 use App\Models\Lottery;
+use App\Models\LotteriesArchive;
 use App\Models\Tickets;
 use App\Domains\Auth\Models\User;
 use App\Notifications\winnerMsg;
@@ -38,7 +39,7 @@ Route::get('draw/{id}', function ($id) {
 	}
 
 	// меняем статус лотереи на завершенную
-	// $lottery->status = 'ended';
+	$lottery->status = 'ended';
 	$lottery->save();
 	// поиск победителя
 	$lottery->winner = Tickets::select('*')
@@ -56,12 +57,40 @@ Route::get('draw/{id}', function ($id) {
 
 	// помечаем билет выигрышным
 	$ticket = Tickets::findOrFail($lottery->winner['id']);
-	// $ticket->status = 'winner';
+	$ticket->status = 'winner';
     $ticket->save();
-    // положить билеты этой лотереи в таблицу архива лотерей, у которых не забрали выигрыш
 
-    // положить лоттерею в архива лотерей
+    // удалить все невыигрышные билеты
+    Tickets::where('lottery_id', $id)
+    ->where('status', '!=', 'winner')
+    // ->get();
+    ->delete();
+    // положить лотерею в архив лотерей
+    LotteriesArchive::create([
+    		'id' => $id,
+            'name' => $lottery->name,
+            'urlKey' => $lottery->urlKey,
+            'starts_at' => $lottery->starts_at,
+            'ends_at' => now(),
+            'price' => $lottery->price,
+            'description' => $lottery->description,
+            'winner_user_id' => $lottery->winner['user_id'],
+            'winner_ticket_id' => $lottery->winner['id'],
+        ]);
+    // создать новую лотерею на место старой
+    Lottery::create([
+           	'name' => $lottery->name,
+            'urlKey' => $lottery->urlKey,
+            'starts_at' => now(),
+            'ends_at' => now()->subDays(30),
+            'price' => $lottery->price,
+            'status' => 'on',
+            'description' => $lottery->description,
+            'users_count' => $lottery->users_count,
+        ]);
 
+    // удаляем лотерею, если все сработало
+    $lottery->delete();
 
     return view('backend.draw',[ 
     	'lottery' => $lottery,
