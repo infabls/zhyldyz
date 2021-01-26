743ae142f9bd97ce61b91fc7c6c90bb415727dcf
infabls <infabls.com@gmail.com>
2021-01-26 22:44:30 +0600

    add buy ticket form to lottery page

--- resources/views/lottery.blade.php
+++ resources/views/lottery.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Все лотереи от компании dfw.kz')
 
 @section('content') 
-    <div id="app" class="flex-center position-ref full-height">
+{{--    <div id="app" class="flex-center position-ref full-height">
             <div class="top-right links">
                 @auth
                     @if ($logged_in_user->isUser())
@@ -18,31 +18,64 @@
                         <a href="{{ route('frontend.auth.register') }}">@lang('Register')</a>
                     @endif
                 @endauth
-            </div><!--top-right-->
+            </div><!--top-right--> --}}
 
             <div class="content">
                 @include('includes.partials.messages')
 
 
-                @isset($ended)
-                    @if ($ended)
-                    <h1 style="color: red">ЗАВЕРШЕНА</h1>
-                    @endif
-                @endisset
-                
-                <h1>Название: {{$lottery->name}}  </h1>
-                <p>Описание: {{$lottery->description}}</p>
-                @if ($lottery->ticket_count > 0)
-                <p>Количество участников: {{$lottery->ticket_count}}</p>
-                @else
-                <p>Пока ни одного участника. Хотите стать первым?</p>
-                @endif
-                <p>Дата начала: {{$lottery->starts_at}}</p>
-                <p>Дата окончания: {{$lottery->ends_at}}</p>
-                <p>Цена участия: {{$lottery->price}}</p>
-                <p>Уже денег: {{$lottery->price * $lottery->ticket_count * 0.8}}</p>
 
-                    <div class="btn btn-danger"><a href="/lottery/{{$lottery->id}}/create">Участвовать</a></div>
+                
+    <div class="container">
+        <div class="pricing">
+        <div class="row">
+            <div class="col-md-6">
+                <article class="animate fadeIn" data-wow-delay="0.4s">
+                    <div class="price-head"> <span class="plan-tittle">Лотерея</span> <span class="plan-price">{{$lottery->price}}<span>тг</span></span> </div>  
+                                    @isset($ended)
+                                @if ($ended)
+                                <h1 style="color: red">ЗАВЕРШЕНА</h1>
+                                @endif
+                            @endisset
+                    <ul>
+                      <li>{{$lottery->description}}</li>
+                      @if ($lottery->ticket_count > 0)
+                      <li>Количество участников: {{$lottery->ticket_count}}<i class="fa fa-check"></i></li>
+                      @else
+                      <li>Пока ни одного участника. Хотите стать первым?<i class="fa fa-check"></i></li>
+                      @endif
+                      <li><p>Дата начала: {{$lottery->starts_at}}</p><i class="fa fa-check"></i></li>
+                      <li><p>Розыгрыш: {{$lottery->ends_at}}</p><i class="fa fa-check"></i></li>
+                      <li><p>Цена участия: {{$lottery->price}}</p><i class="fa fa-check"></i></li>
+                      <li><p>Уже денег: {{$lottery->price * $lottery->ticket_count * 0.8}}</p><i class="fa fa-check"></i></li>
+                    </ul>
+                    <a href="/lottery/{{$lottery->id}}/create" class="btn btn-inverse">Участвовать</a>
+                </article>
+            </div>
+            <div class="col-md-6">
+                <h1>Форма   {{$lottery->name}}</h1>
+                <form name="add-blog-post-form" id="add-blog-post-form" method="post" action="{{url()->current()}}">
+                @csrf
+                <div class="box_detail booking">
+                    <div class="form-group" id="input-dates">
+                        <div class="form-group">
+                            <label for="code">Ваш код</label>
+                            <input  name="code" type="text" class="form-control" id="code" required="" placeholder="Введите код из 10 символов" minlength='10' maxlength='10'>
+                        </div>
+                    </div>
+                    <div class="form-group" id="input-dates">
+                        <div class="form-group">
+                            <label for="phone">Номер телефона</label>
+                            <input  name="phone" type="tel" class="form-control" id="phone" placeholder="+7 777 333 45 67">
+                        </div>
+                    </div>
+                    <button type="submit" class="add_top_30 btn_1 full-width purchase">Купить билет</button>
+                    <div class="text-center"><small>Обязательно сохраните код - иначе горох!</small></div>
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
 
 
             </div><!--content-->
